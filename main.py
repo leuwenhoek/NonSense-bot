70e1cf8b20c7e5b000ae720c7463a9f5197f4160
leuwenhoek <ninjabeastyy24@gmail.com>
2026-01-01 03:20:29 +0530

    segregated active_listen()

--- main.py
+++ main.py
@@ -28,33 +28,35 @@ class NonSense_Bot:
         except Exception as e:
             print(f"Speech error: {e}")
 
-    def listening(self):
+    
 
-        def active_listen():
+        def active_listen(self):
             with sr.Microphone() as mic:
                 print("\nListening for 'nonsense'....")
                 self.r.adjust_for_ambient_noise(mic, duration=0.5)
                 audio = self.r.listen(mic)
-                return audio
+                audio_text = self.r.recognize_google(audio).lower()
+                return audio_text
+
+    def listening(self):
 
         while True:
-                audio = active_listen()
+                audio_text = self.active_listen()
 
                 try:
-                    audio_text = self.r.recognize_google(audio).lower()
-                    print(f"Recognized: {audio_text}")
 
                     if Activation_word in audio_text:
                         print("System: Activation word detected!")
-                        self.speak("How can I help you?")
+                        self.speak("System Activate, Nonsense-Bot here")
+                        break
                     else:
-                        print(f"Repeating: {audio_text}")
-                        self.speak(audio_text)
+                        pass
 
                 except sr.UnknownValueError:
                     pass
                 except Exception as e:
                     print(f"Sorry error occurred: {e}")
+
 
 def main():
     NS_bot = NonSense_Bot()
