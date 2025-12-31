0ab5340bc494150071d70e1906bbd6b7df107491
leuwenhoek <ninjabeastyy24@gmail.com>
2026-01-01 03:54:17 +0530

    ADDED: error handling in active listen function and able to load API_KEY of gemini 1.5

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import speech_recognition as sr
+import google.generativeai as genai
+from dotenv import load_dotenv
 from gtts import gTTS
 import pygame
 import os
 import time
 
 Activation_word = "nonsense"
+
+load_dotenv()
+API_KEY = os.environ.get("api_key")
 
 class NonSense_Bot:
     def __init__(self):
@@ -32,10 +37,19 @@ class NonSense_Bot:
 
     def active_listen(self):
         with sr.Microphone() as mic:
-            print("\nListening for 'nonsense'....")
+            print("\nListening...")
             self.r.adjust_for_ambient_noise(mic, duration=0.5)
             audio = self.r.listen(mic)
-            audio_text = self.r.recognize_google(audio).lower()
+            
+            audio_text = "" 
+
+            try:
+                audio_text = self.r.recognize_google(audio).lower()
+            except sr.UnknownValueError:
+                print("Recognition failed: I didn't catch that.")
+            except sr.RequestError:
+                print("Network error: Check your internet.")
+            
             return audio_text
 
     def listening(self):
@@ -58,11 +72,19 @@ class NonSense_Bot:
                     print(f"Sorry error occurred: {e}")
 
     def reply(self):
-        self.a
+        while True:
+            said = self.active_listen()
+            
+
+    def run(self):
+        while True:
+            self.listening()
+            self.reply()
 
 def main():
     NS_bot = NonSense_Bot()
-    NS_bot.listening()
+    NS_bot.run()
+
 
 if __name__ == "__main__":
     main()
